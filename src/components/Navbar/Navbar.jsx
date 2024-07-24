717b419ae57007aff0ab039df22d0e6ae2698747
Taybm16 <136743688+Taybm16@users.noreply.github.com>
2024-07-24 19:14:45 -0300

    Navbar atualizada

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import "./Navbar.scss"
 import NerdHouse from "../../assets/Images/Nerd-house-logo.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope,faHeartCirclePlus } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope,faBook,faHeartCirclePlus,faTabletScreenButton,faAddressBook,faCircleUser,faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar(){
 
@@ -11,12 +11,12 @@ function Navbar(){
             <div id = "icones">
                 
                 <button  > <FontAwesomeIcon icon={faEnvelope} /> <span>Home</span> </button>
-                <button ><FontAwesomeIcon icon={faHeartCirclePlus} /> <span>Livraria</span></button>
+                <button ><FontAwesomeIcon icon={faBook} /><span>Livraria</span></button>
                 <button ><FontAwesomeIcon icon={faHeartCirclePlus} /><span>Favoritos</span></button>
-                <button ><span>Ebooks</span></button>
-                <button ><span>Portfolios</span></button>
-                <button ><span>Login</span></button>
-                <button ><span>Carrinho</span></button>
+                <button ><FontAwesomeIcon icon={faTabletScreenButton} /> <span>Ebooks</span></button>
+                <button ><FontAwesomeIcon icon={faAddressBook} /><span>Portfolios</span></button>
+                <button ><FontAwesomeIcon icon={faCircleUser} /><span>Login</span></button>
+                <button ><FontAwesomeIcon icon={faCartShopping} /><span>Carrinho</span></button>
                 
                 
                 
