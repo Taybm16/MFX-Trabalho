cad991ea8c4795287ae6efa641ef67e80d249624
Taybm16 <136743688+Taybm16@users.noreply.github.com>
2024-07-26 21:25:30 -0300

    Quem somos

---
+++ src/components/QuemSomos/QuemSomos.jsx
@@ -0,0 +1,42 @@
+import "./QuemSomos.css"
+
+function QuemSomos(){
+    return(
+        <>
+        <h2>
+            Quem Somos:
+                Bem-vindo à Nerd House, o seu refúgio de conteúdo! 
+
+                Na Nerd House, acreditamos que o conhecimento e a criatividade são forças transformadoras. Nosso site é um ponto de encontro para entusiastas de todos os tipos, oferecendo uma plataforma única onde você pode tanto consumir quanto produzir conteúdo de alta qualidade.
+
+                Nossa Missão:
+
+                Nossa missão é empoderar criadores e consumidores de conteúdo, proporcionando um espaço dinâmico e acessível para todos. Queremos facilitar o acesso a recursos educacionais e de entretenimento, promovendo a troca de ideias e o crescimento intelectual.
+
+                O que Oferecemos:
+
+                Na Nerd House, você encontra uma variedade de serviços e produtos que atendem a todas as suas necessidades de conteúdo:
+
+                Livraria: Explore uma vasta seleção de livros, desde clássicos da literatura até os mais recentes lançamentos.
+                Audiobooks: Desfrute de histórias narradas, perfeitas para ouvir em qualquer lugar.
+                Ebooks: Tenha acesso a uma biblioteca digital completa para ler no seu dispositivo preferido.
+                Portfólios: Descubra e conecte-se com criadores de conteúdo talentosos, visualizando seus trabalhos em um só lugar.
+                Login: Tenha acesso personalizado ao nosso site, com funcionalidades exclusivas para membros.
+                Carrinho: Facilite suas compras com nosso sistema de carrinho de compras integrado.
+                
+                Para Criadores:
+
+                Se você é um criador de conteúdo, a Nerd House é o lugar ideal para você. Oferecemos a possibilidade de vender seus conteúdos ou publicá-los gratuitamente, permitindo que você alcance um público maior e monetize suas criações.
+
+                Junte-se a Nós
+
+                Quer você esteja procurando por novos conhecimentos, entretenimento de qualidade, ou um local para compartilhar suas criações, a Nerd House é o seu destino. Junte-se à nossa comunidade hoje mesmo e faça parte de um movimento que valoriza o poder do conteúdo.
+
+</h2>
+        </>
+        
+
+    )
+}
+
+export default QuemSomos;
